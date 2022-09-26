aa2a7b29cb27169f22a2609cc60592b18c62df8d
ESHEKU ESHEMOKHAI <44941166+esheks@users.noreply.github.com>
2022-09-26 18:38:19 -0500

    Update common.groovy

--- vars/common.groovy
+++ vars/common.groovy
@@ -2,14 +2,19 @@ def call(String stageName){
   
   if ("${stageName}" == "Build")
      {
+       //sh "echo 'running JUnit-test-cases' "
+       //sh "echo 'testing must passed to create artifacts ' "
        sh "mvn clean package"
      }
   else if ("${stageName}" == "SonarQube Report")
      {
+        //sh "echo 'Perfoming CodeQualityAnalysis' "
+        //sh "mvn sonar:sonar"
        sh "mvn clean sonar:sonar"
      }
   else if ("${stageName}" == "Upload Into Nexus")
      {
+       //sh "saving artifact' "
        sh "mvn clean deploy"
      }
 }
